77f7b8532760851ff6da9b34b2bb164e1ccb1b62
harshshah78910 <58108585+harshshah78910@users.noreply.github.com>
2020-02-03 10:14:12 -0500

    Calculate Factorial
    Recursion Example

---
+++ FactorialRec.java
@@ -0,0 +1,17 @@
+
+public class FactorialRec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println(fact(4));
+	}
+	
+	public static int fact(int n)
+	{
+		if(n == 0 || n == 1)
+			return 1;
+		else
+			return n * fact(n-1);
+	}
+
+}
