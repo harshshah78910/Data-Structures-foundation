51e6640686a5d586cfb558fd7abb193229508a09
harshshah78910 <58108585+harshshah78910@users.noreply.github.com>
2020-02-04 13:44:05 -0500

    Insertion Sort
    Inserion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[] { 2, 1, 3, 5, 100, 7 };
+		doInsertionSort(a);
+		for (int p : a)
+			System.out.print(p + " ");
+	}
+	
+	private static void doInsertionSort(int[] arr)
+	{
+		for(int i=1 ; i<arr.length ; i++)
+		{
+			int value = arr[i];
+			int holeIndex = i;
+			while(holeIndex > 0 && arr[holeIndex-1] > value)
+			{
+				arr[holeIndex] = arr[holeIndex-1];
+				holeIndex--;
+			}
+			arr[holeIndex] = value;
+		}
+	}
+
+}
