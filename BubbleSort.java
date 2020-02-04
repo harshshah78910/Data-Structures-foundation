8b2c789a37b40c810e47acf9d29003d133542f91
harshshah78910 <58108585+harshshah78910@users.noreply.github.com>
2020-02-04 12:40:45 -0500

    Bubble Sort
    Optimization to break out of loop if no swaps has occured in the step

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,17 +5,24 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr[] = new int[] { 12, 2, 3, 45, 654, 3 };
+		int arr[] = new int[] { 1,2,3 };
 		doBubbleSort(arr);
 		for (int p : arr)
 			System.out.print(p + " ");
 	}
 
 	private static void doBubbleSort(int[] arr) {
+		boolean flag = false;
 		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = 0; j < arr.length - i -1; j++) {
-				if (arr[j] > arr[j + 1])
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
 					swap(j, j + 1, arr);
+				}
+
+				flag = true;
+			}
+			if (!flag) {
+				break;
 			}
 		}
 	}
