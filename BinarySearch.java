68d5f7aa2458ccbd03a344b2ad2eee5fd9ca371b
harsh <harsh@DESKTOP-G850PQB.fios-router.home>
2020-02-06 19:21:06 -0500

    Binary Search : Recursion and Iteration

---
+++ BinarySearch.java
@@ -0,0 +1,57 @@
+
+public class BinarySearch {
+
+	public static void main(String[] args) 
+	{
+		int arr[] = new int[] { 1,2,3,34,56,200};
+	    System.out.println(doBinarySearchRec(arr,0,arr.length,23));
+	    System.out.println(doBinarySearchIterative(arr,34));
+	}
+	
+	
+	private static int doBinarySearchIterative(int[] arr, int x)
+	{
+		int start = 0; 
+		int end = arr.length-1;
+		int mid = (start+end)/2;
+		while(start <= end)
+		{
+			if(arr[mid] == x)
+				return mid;
+			else if(arr[mid] > x)
+			{
+				start = start;
+				end = mid -1;
+			}
+			else 
+			{
+				start = mid +1;
+				end = end;
+			}
+			mid = (start + end)/2;
+		}
+		return -1;
+	}
+	
+	private static int  doBinarySearchRec(int[] arr, int start, int end, int x)
+	{
+		if(start > end)
+		{
+			return -1;
+		}
+		int mid = (start+end)/2;
+		if(arr[mid] == x)
+		{
+			return mid;
+		}
+		else if(arr[mid] > x)
+		{
+			return doBinarySearchRec(arr, start, mid-1, x);
+		}
+		else
+		{
+			return doBinarySearchRec(arr, mid+1, end, x);
+		}
+	}
+
+}
