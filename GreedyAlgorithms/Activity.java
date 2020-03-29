be3c0e008c7a569f4c1a22f413d8e2d1a5e147ef
harsh <harsh@DESKTOP-G850PQB>
2020-03-29 11:28:37 -0400

    Greedy Folder and Algo

---
+++ GreedyAlgorithms/Activity.java
@@ -0,0 +1,64 @@
+package GreedyAlgorithms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Activity {
+
+	public static void main(String[] args) {
+		
+		int[] start = {1, 3, 0, 5, 8, 5};
+		int[] end = {2, 4, 6, 7, 9, 9};
+		
+		System.out.println(activitySelection(start, end, 6));
+
+	}
+
+	static int activitySelection(int start[], int end[], int n) {
+		class Job {
+			int startTime;
+			int finishTime;
+			int originalIndex;
+		}
+
+		ArrayList<Job> jobList = new ArrayList<Job>();
+
+		for (int i = 0; i < start.length; i++) {
+			Job j = new Job();
+			j.startTime = start[i];
+			j.finishTime = end[i];
+			j.originalIndex = i;
+			jobList.add(j);
+		}
+
+		Collections.sort(jobList, new Comparator<Job>() {
+			@Override
+			public int compare(Job j1, Job j2) {
+
+				return j1.finishTime - j2.finishTime;
+			}
+
+		});
+
+		Job initialJob = jobList.remove(0);
+		int currentFinishTime = initialJob.finishTime;
+		ArrayList<Job> result = new ArrayList<Job>();
+		result.add(initialJob);
+		
+		while(!jobList.isEmpty())
+		{
+			Job currentJob = jobList.remove(0);
+			if(currentJob.startTime >= currentFinishTime)
+			{
+				currentFinishTime = currentJob.finishTime;
+				result.add(currentJob);
+			}
+		}
+		
+		for(Job j: result)
+			System.out.print(j.originalIndex+1 + " ");
+		
+		return 0;
+
+	}
+}
